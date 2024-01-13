f11d779f87baa3db582c236be7d6885e591f2a9a
NotWither <152763733+NotWither@users.noreply.github.com>
2024-01-13 16:34:03 +0530

    Add files via upload

---
+++ virtual environment/141.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup as bs
+import requests 
+import pandas as pd 
+StarUrl = "https://en.wikipedia.org/wiki/List_of_nearest_giant_stars"
+
+page=requests.get(StarUrl)
+soup = bs(page.text,"html.parser")
+startable=soup.find('table')
+templist=[]
+tablerows=startable.find_all('tr')
+for  tr in tablerows:
+    td=tr.find_all('td')
+    row=[i.text.rstrip()for i in td]
+
+    templist.append(row)
+    print (templist)
+starsname=[]
+distance=[]
+mass=[]
+radius=[]    
+for i in  range (1,len(templist)):
+    starsname.append(templist [i][1])
+    distance.append(templist [i][3])
+    mass.append(templist [i][5])
+    radius.append(templist [i][4])
+headers=["starsname","distance","mass","radius"]
+df=pd.DataFrame(list(zip (starsname,distance,mass,radius)),columns=headers)
+df.to_csv('stars.csv')
